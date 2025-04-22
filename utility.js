1c5838c69376e25e2c7fc361c3ba562192053384
technoalli3 <chloebenner1900@gmail.com>
2025-04-22 14:58:10 -0400

    added cursors

--- utility.js
+++ utility.js
@@ -3,5 +3,14 @@
 function start() {
 	document.getElementById("name").textContent = "TechnoAlli3";
 	console.log("I have run");
+	
+	/*let projectCards = document.getElementsByClassName("project-grid-container");
+	for(let i = 0; i < projectCards.length; i++) {
+		projectCards[i].addEventListener("mouseover", function(e) {
+			console.log("I have run");
+			e.style.transform = `rotate(${70}deg)`;
+		});
+	}*/
 }
+
 window.onload = start;
